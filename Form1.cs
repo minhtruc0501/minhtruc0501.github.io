41d4c9cdb37cdc19afca46e01cc1ca540b5bdb97
minhtruc0501 <trucpkk133@gmail.com>
2023-07-17 09:20:57 +0700

    first commit

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex3
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+        string path=@"D:\form.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Write(InfoWindows iw){
+            XmlSerializer writer = new XmlSerializer(typeof(InfoWindows));
+            StreamWriter file = new StreamWriter(path);
+            writer.Serialize(file,iw);
+            file.Close();}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+
+        }
+    }
+string path=@"D:\form.xml";
+
+public void Write(InfoWindows iw){
+    XmlSerializer writer = new XmlSerializer(typeof(InfoWindows));
+    StreamWriter file = new StreamWriter(path);
+    writer.Serialize(file,iw);
+    file.Close();
+    }
+}
---
+++ InfoWindows.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex3
+{
+    class InfoWindows
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+      
+   
+}
